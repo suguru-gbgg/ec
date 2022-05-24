a85cb39aa609c4973959056ae24c27a08b2ecba8
suguru-gbgg <takagisn12@gmail.com>
2022-05-24 20:17:01 +0900

    フロントとバックエンドを分けました

--- front/syousai.php
+++ front/syousai.php
@@ -1,14 +1,12 @@
 <?php
-    
-    $pdo = new PDO('mysql:host=localhost;dbname=ec;','root','');
-    $sql = "SELECT * FROM `items` WHERE id =" .$_GET['id'];
-    $datas = $pdo -> query($sql);
-    foreach($datas as $data){
+    require "../server/get_syohin.php";
+    $get_syohin = new syohin;
+    $datas = $get_syohin -> get_syohin_syousai($_GET["id"]);
 
+    foreach($datas as $data){
         echo $data["item_name"];
         echo $data["item_contents"];
         echo $data["item_value"];
-
     }
 
 ?>
