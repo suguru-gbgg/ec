5322b2858635b19746c96013c08ae9ad779df6a7
suguru-gbgg <takagisn12@gmail.com>
2022-05-25 21:32:08 +0900

    ユーザー認証実装

---
+++ front/erro_signup_id.php
@@ -0,0 +1,6 @@
+<html>
+
+    <a>IDが重複しています</a>
+    <a href="signup.php">戻る</a>
+
+</html>
--- server/get_signup.php
+++ server/get_signup.php
@@ -1,8 +1,28 @@
 <?php
 
+    if(empty($_GET["user_id"])||empty($_GET["pass"])){
+            
+        header('Location: http://localhost/work/ec/front/erro_signup.php');
+        exit;
+
+    }
+
+    $pdo = new PDO('mysql:host=localhost;dbname=ec;','root','');
+    $sql = "SELECT `user_id` FROM `users` WHERE user_id ='".$_GET['user_id']."'";
+    echo $sql;
+    $datas = $pdo -> query($sql);
+    
+    foreach($datas as $data){
+        if($data["user_id"] == $_GET["user_id"]){
+            header('Location: http://localhost/work/ec/front/erro_signup_id.php');
+            exit;
+        } 
+    }
+
     $pdo = new PDO('mysql:host=localhost;dbname=ec;','root','');
     $sql = "INSERT INTO `users`(`user_id`, `pass`) VALUES ('".$_GET['user_id']."','".$_GET['pass']."')";
     $pdo -> query($sql);
-    header('Location: http://localhost/work/ec/front/signin.php');
+    header('Location: http://localhost/work/ec/front/signin.php'); 
+    exit;
 
 ?>
